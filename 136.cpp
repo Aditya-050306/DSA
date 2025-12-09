4fd935919d1eab2b6ad60678cb9789a7a4a49864
Aditya-050306 <joshiaditya0503@gmail.com>
2025-12-09 19:02:47 +0530

    Single Number Problem in Leetcode

--- 136.cpp
+++ 136.cpp
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int result=0;
         for(int i: nums){
-            result=result^i;
+            result=result^i;    //Bitwise XOR operation     n^n=0     n^0=n
         }
         return result;
     }
