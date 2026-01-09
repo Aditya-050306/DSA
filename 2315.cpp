f9a6b0a3d89e5a5a316b8e6dff788e9030a2ec09
Aditya-050306 <joshiaditya0503@gmail.com>
2026-01-09 23:24:14 +0530

    Count Asterisks-Leetcode

--- 2315.cpp
+++ 2315.cpp
@@ -4,10 +4,10 @@ public:
         int count=0;
         bool insidePair=false;   //this helps to determine we are inside the pair or not
         for(char c:s){
-            if(c=="|"){
+            if(c=='|'){
                 insidePair=!insidePair;    //converts false to true and vice versa... false->we are outside true-> we are inside
             }
-            else if(c=="*" && !insidePair){   //if c=="*" && insidePair=false then count else skip
+            else if(c=='*' && !insidePair){   //if c=="*" && insidePair=false then count else skip
                 count++;
             }
         }
