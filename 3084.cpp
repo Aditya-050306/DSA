4a1b69f5bf4d3eadc0324d77723a38e9e4ae2b7f
Aditya-050306 <joshiaditya0503@gmail.com>
2026-08-07 22:43:04 +0530

    Count Substrings Starting and Ending with Given Character- Leetcode

---
+++ 3084.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long countSubstrings(string s, char c) {
+        long long n=0;
+        for(char ch:s){
+            if(ch==c){
+                n++;
+            }
+        }
+        return n*(n+1)/2;
+    }
+};
